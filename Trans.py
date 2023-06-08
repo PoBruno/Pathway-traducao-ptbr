7c1689dd24a2048aea2328401fdeeb5bf96197c4
Bruno Gomes <brunoshy@gmail.com>
2023-06-08 01:28:29 -0300

    Finalização da Tradução

--- Trans.py
+++ Trans.py
@@ -1,13 +1,23 @@
 import pandas as pd
-from googletrans import Translator as T
-import time
-import tqdm.notebook as tq
+from googletrans import Translator
 
-CNAE = pd.read_excel('/content/correspondecia_CPV_CNAE.xlsx', 'CNAE', dtype=str)
-CPV = pd.read_excel('/content/correspondecia_CPV_CNAE.xlsx', 'CPV2003', dtype=str)
-CNAE.head()
+# Carregando o arquivo XLS
+file_path = "./pt-BR/Events.xlsx"
+df = pd.read_excel(file_path)
+
+# Criando uma instância do tradutor
+translator = Translator(service_urls=['translate.google.com'])
+
+# Função para traduzir o texto
+def translate_text(text):
+    translation = translator.translate(text, src='en', dest='pt')
+    return translation.text
 
 
+# Traduzindo os valores da coluna "en-US" e inserindo na coluna "pt-BR"
+df['pt-BR'] = df['en-US'].apply(translate_text)
 
-
+# Salvando o arquivo XLS traduzido
+output_file_path = "./pt-BR/Events_translated.xlsx"
+df.to_excel(output_file_path, index=False)
 
